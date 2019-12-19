bca4bd3a3c6b355ed15c5b2d98fc4b9b2ea076eb
Sara Garza <sjgarza22@gmail.com>
2019-12-19 21:42:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,10 +3,14 @@ def oxford_comma(array)
 
   when  1
     puts array.first
+    array
   when 2
     puts array.join(" and ")
+    array
   else
     array[-1] = array[-1].unshift("and ")
     string = array.join(", ")
+    puts string
+    array
   end
 end
