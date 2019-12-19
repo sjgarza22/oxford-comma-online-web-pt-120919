247d562e5a42dcc4c31d951e7e33e823f717ada1
Sara Garza <sjgarza22@gmail.com>
2019-12-19 22:00:28 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  case array.length
 
+  when  1
+    string = array.first
+    puts string
+    string
+  when 2
+    string = array.join (" and ")
+    puts string
+    string
+  else
+    array[-1].insert(0, "and ")
+    string = array.join(", ")
+    puts string
+    string
+  end
 end
